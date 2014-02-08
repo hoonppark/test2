af9b0926184853866d30c8f5528bb463075c0e8b
박훈병 <hoonbpark@ubidom.com>
2014-02-08 15:04:46 +0900

    modified HelloWorld3.java

--- src/HelloWorld3.java
+++ src/HelloWorld3.java
@@ -1,7 +1,7 @@
 
 public class HelloWorld3 {
 	public String sayHello3(){
-		System.out.println("This is HelloWorld3.sayHello3() function");
+		System.out.println("for testBranch-2 : This is HelloWorld3.sayHello3() function");
 		return "Hello from HelloWorld3()";
 		
 	}
