35f06d2ed244619ba39a79779038550bffb3e02c
박훈병 <hoonbpark@ubidom.com>
2014-02-08 15:22:09 +0900

    modified HelloWorld3.java in testBranch-1

--- src/HelloWorld3.java
+++ src/HelloWorld3.java
@@ -1,7 +1,11 @@
 
 public class HelloWorld3 {
 	public String sayHello3(){
+<<<<<<< HEAD
 		System.out.println("for testBranch-2 : This is HelloWorld3.sayHello3() function");
+=======
+		System.out.println("testBranch-1: modified in testBranch-1: This is HelloWorld3.sayHello3() function");
+>>>>>>> modified HelloWorld3.java in testBranch-1
 		return "Hello from HelloWorld3()";
 		
 	}
