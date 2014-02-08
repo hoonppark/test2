347610e1ea78d78edebf497f3ee594814195b179
박훈병 <hoonbpark@ubidom.com>
2014-02-08 13:38:31 +0900

    Added HelloWorld4.java - just for testBranch-1

---
+++ src/HelloWorld4.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld4 {
+	public String sayHello4(){
+		System.out.println("Added for 'testBranch-1': This is HelloWorld4.sayHello4() function");
+		return "Hello from HelloWorld34()";
+		
+	}
+}
