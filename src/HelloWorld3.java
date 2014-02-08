c2b2e12fc7440a87a5e7b608095f21f9b07c29e1
박훈병 <hoonbpark@ubidom.com>
2014-02-08 15:26:26 +0900

    cleaned up HelloWorld3.java

--- src/HelloWorld3.java
+++ src/HelloWorld3.java
@@ -1,11 +1,9 @@
 
 public class HelloWorld3 {
 	public String sayHello3(){
-<<<<<<< HEAD
-		System.out.println("for testBranch-2 : This is HelloWorld3.sayHello3() function");
-=======
-		System.out.println("testBranch-1: modified in testBranch-1: This is HelloWorld3.sayHello3() function");
->>>>>>> modified HelloWorld3.java in testBranch-1
+	
+		System.out.println("for testBranch-1 and testBranch-2 : This is HelloWorld3.sayHello3() function");
+
 		return "Hello from HelloWorld3()";
 		
 	}
